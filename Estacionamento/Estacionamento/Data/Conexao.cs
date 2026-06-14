901bab86d3c785b26d5dccfacd0288117d55a918
Gabrieljdhiujd38ui3e9o <ga9617097@gmail.com>
2026-06-14 15:45:19 -0300

    Format Conexao.cs for consistency

--- Estacionamento/Estacionamento/Data/Conexao.cs
+++ Estacionamento/Estacionamento/Data/Conexao.cs
@@ -6,7 +6,7 @@ namespace Estacionamento.Data
     {
         public SqlConnection PegarConexao()
         {
-            // Substitua localhost pelo nome do seu banco se precisar (ex: .\\SQLEXPRESS)
+            
             string strConexao = "Server=.\\SQLEXPRESS;Database=EstacionamentoDB;Trusted_Connection=True;TrustServerCertificate=True;";
             SqlConnection conexao = new SqlConnection(strConexao);
             return conexao;
